fe0c7492ee79cf3c23f2117e36ea89346a0c3d8f
Ubuntu <ubuntu@ip-172-31-9-35.eu-west-1.compute.internal>
2014-11-02 07:40:25 +0000

    added db to db.sql

--- db.sql
+++ db.sql
@@ -168,16 +168,18 @@ ALTER TABLE ONLY "user" ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regcl
 --
 
 COPY company (id, name, description, twitter, founded_year, founders, url, bitly_url, logo_submited, logo, contact_email, contact_name, date_submit, status) FROM stdin;
-9	Campadillo		@campadillo	2014	\N	http://campadillo.com	\N	http://campadillo.com/img/campadillo_logo_white.png	90ac3d6b-a1ba-4eca-8dca-f879037dfd8e.png	office@campadillo.com	Cosmin Stamate	2014-10-23 22:59:31	accepted
 10	7Out		@7OutApp	1999	\N	http://get7out.com	\N	http://get7out.com/press-kit.zip	8c35f491-4020-453c-b01f-eb45d589888c.png	support@7out.us	support	2014-10-25 15:36:00	accepted
-1	FaceRig		@FaceRig	2013	\N	http://www.facerig.com	\N	http://facerig.com/images/downloads/AvatarScreenshots_8.zip	54534b25-de9a-4c22-80f8-dfb91ef8341b.png	dont@know.yet	Don't know	2014-10-14 13:28:14	pending
 12	Eventya		@eventyaeu	1991	\N	http://eventya.eu/	\N	http://cldup.com/FsMoB3eCe2.png	83bff4e8-da74-4b7f-9e57-01d56c672fb6.png	imunteanu@i-consult.ro	Ionuț Munteanu	2014-10-28 23:15:06	accepted
 11	GloriaFood		@gloriafood	2013	\N	http://www.gloriafood.com	\N	https://www.gloriafood.com/media-kit	f89601e0-962d-4552-8840-2d57f9899afd.png	oliver.auerbach@gloriafood.com	Oliver Auerbach	2014-10-28 15:17:05	accepted
 4	CyberGhost		@cyberghost_EN	2011	\N	http://www.cyberghostvpn.com	\N	http://www.cyberghostvpn.com/download/press/cg5_press_kit_en.rar	809ad3ad-8366-40b6-b565-7d8de001d788.png	press.office@cyberghost.ro	Press	2014-10-14 13:39:10	accepted
 2	Two Tap		@twotapbuy	2013	\N	http://twotap.com	\N	http://twotap.com/public/twotap-mediakit.zip	28398549-8f2c-4ccb-afbc-77e27a904545.png	press@twotap.com	Press	2014-10-14 13:25:37	accepted
 6	Juqster		@Juqster	2014	\N	http://www.juqster.com	\N	http://www.juqster.com/imgs/JuqsterLogo1200x1200WhiteBackground.png	f8b0f165-0a6f-4fab-a62b-e460ef2488ed.png	radu@juqster.com	Tapus Radu	2014-10-16 13:45:57	accepted
 3	Tintag		@Tintagapp	1999	\N	http://thetintag.com/	\N	http://thetintag.com/TintagPressKit.zip	258288b8-d165-44cc-b3d0-599da09c05b4.png	office@tint.ag	Office	2014-10-14 13:35:11	accepted
+1	FaceRig		@FaceRig	2013	\N	http://www.facerig.com	\N	http://facerig.com/images/downloads/AvatarScreenshots_8.zip	43f2ee3d-14dc-466c-a7f9-cc62fb55d252.png	press@holotech.eu	Florin Stanculescu	2014-10-14 13:28:14	accepted
 8	Moqups		@moqups	2012	\N	https://moqups.com	\N	https://github.com/Evercoder/mediakit	2285ca13-2277-45ef-bfc8-41fe662ffa80.png	emil@moqups.com	Emil Tamaș	2014-10-22 13:43:14	accepted
+13	Omnipaste		@omnipaste	2014	\N	https://www.omnipasteapp.com	\N	https://www.dropbox.com/s/967gv8w4z8rivw3/logo_white.png?dl=0	544ac70d-a761-4b36-9922-fb50286181d5.png	calin@omnipasteapp.com	Alexandru Calinoiu	2014-10-31 20:58:33	accepted
+9	Campadillo		@campadillo	2014	\N	http://campadillo.com	\N	http://campadillo.com/img/campadillo_logo_white.png	d0f420b4-15d1-4952-ac7e-cb007f22c065.png	office@campadillo.com	Cosmin Stamate	2014-10-23 22:59:31	accepted
+14	REBS	\N	none	2013	\N	http://rebs-group.com	\N	http://i.imgur.com/fDBzhpg.png	\N	dumitru.sirbu@rebs-group.com	Dumitru Sirbu	2014-11-02 01:00:42	pending
 \.
 
 
@@ -185,7 +187,7 @@ COPY company (id, name, description, twitter, founded_year, founders, url, bitly
 -- Name: company_id_seq; Type: SEQUENCE SET; Schema: public; Owner: ubuntu
 --
 
-SELECT pg_catalog.setval('company_id_seq', 12, true);
+SELECT pg_catalog.setval('company_id_seq', 14, true);
 
 
 --
@@ -193,7 +195,7 @@ SELECT pg_catalog.setval('company_id_seq', 12, true);
 --
 
 COPY pair (id, key, val) FROM stdin;
-1	since	1414545306
+1	since	1414897242
 \.
 
 
